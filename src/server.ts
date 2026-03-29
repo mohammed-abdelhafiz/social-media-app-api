8314c7cc395443b980ec72ac6bb3a51a632e9af7
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-03-29 23:14:19 +0200

    fix(server): update PORT initialization to ensure proper fallback value and type conversion

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import "./shared/config/env.config";
 import app from "./app";
 import { connectToDb } from "./shared/config/db.config";
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const PORT = Number(process.env.PORT) || 5000;
 
 const startServer = async () => {
   try {

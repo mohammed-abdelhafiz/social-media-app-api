fc4e3682bba9cd001c9dd50369f4b7b3db803be6
Mohamed Abdelhafiz <mohamed.abdelhafiz.dev@gmail.com>
2026-03-29 22:17:25 +0200

    fix(server): update PORT handling and improve server start logging for better clarity

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,17 @@ import "./shared/config/env.config";
 import app from "./app";
 import { connectToDb } from "./shared/config/db.config";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000; // حوّل string لـ number
 
 const startServer = async () => {
   try {
     await connectToDb();
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     process.exit(1);
   }
 };
